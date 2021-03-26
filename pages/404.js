d871f27e16818db176597d95e5f6b35569b13e9a
EvelymVasconcelos <evelym.vasconcelos@gmail.com>
2021-03-25 20:18:45 -0400

    first commit

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+export default function Page404(){
+    return (
+        <div>Pagina 404</div>
+    )
+}
---
+++ pages/sobre.js
@@ -0,0 +1,5 @@
+export default function PageSobre(){
+    return(
+        <div>Página Sobre</div>
+    )
+}
